7f5ddab9bc56fcad08717c67468c4f98d13b40d2
Asharamart2075@gmail.com <Asharamart2075@gmail.com>
2023-04-13 22:19:40 -0500

    Made some corrections to the example height and width

---
+++ Lib/shapes.js
@@ -0,0 +1,17 @@
+// Following packages needed for this Generator
+const Examples = require('Examples');
+const Example1 = require('Examples\example1Triangle.svg')
+const Example2 = require('Examples\example2Circle.svg')
+const Example3 = require('Examples\example3Square.svg')
+const inquirer = require('inquirer');
+const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+const numberChars = "0123456789";
+const specialChars = "!@#$%^&*()_+{}[]|\\:;\"',.<>/?";
+
+const messages = [
+    "Hello World!",
+    "Welcome to Node.js",
+    "This is a test message"
+  ];
+  
