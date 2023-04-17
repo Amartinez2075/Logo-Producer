af58072c7057acf4884dec9e37f23f49631e7a00
Asharamart2075@gmail.com <Asharamart2075@gmail.com>
2023-04-16 22:31:53 -0500

    added comments

--- Lib/shapes.js
+++ Lib/shapes.js
@@ -1,40 +1,47 @@
+// Define the Triangle class with a constructor that takes in x, y, size, and color
 class Triangle {
   constructor(x, y, size, color) {
-    this.x = x;
-    this.y = y;
-    this.size = size;
-    this.color = color;
+  this.x = x;
+  this.y = y;
+  this.size = size;
+  this.color = color;
   }
-
+  
+  // Define the render method for the Triangle class, which returns a SVG polygon with the given color
   render() {
-    return `<polygon points="150, 18 244, 182 56, 182" fill="${this.color}" />`;
+  return <polygon points="150, 18 244, 182 56, 182" fill="${this.color}" />;
   }
-}
-
-class Square {
+  }
+  
+  // Define the Square class with a constructor that takes in x, y, size, and color
+  class Square {
   constructor(x, y, size, color) {
-    this.x = x;
-    this.y = y;
-    this.size = size;
-    this.color = color;
+  this.x = x;
+  this.y = y;
+  this.size = size;
+  this.color = color;
   }
-
+  
+  // Define the render method for the Square class, which returns a SVG rectangle with the given color
   render() {
-    return `<rect x="50" y="50" width="200" height="200" fill="${this.color}" />`;
+  return <rect x="50" y="50" width="200" height="200" fill="${this.color}" />;
   }
-}
-
-class Circle {
+  }
+  
+  // Define the Circle class with a constructor that takes in x, y, size, and color
+  class Circle {
   constructor(x, y, size, color) {
-    this.x = x;
-    this.y = y;
-    this.size = size;
-    this.color = color;
+  this.x = x;
+  this.y = y;
+  this.size = size;
+  this.color = color;
   }
-
+  
+  // Define the render method for the Circle class, which returns a SVG circle with the given color and radius
   render() {
-    return `<circle cx="${this.x + (this.size / 2)}" cy="${this.y + (this.size / 2)}" r="${this.size / 2}" fill="${this.color}" />`;
+  return <circle cx="${this.x + (this.size / 2)}" cy="${this.y + (this.size / 2)}" r="${this.size / 2}" fill="${this.color}" />;
   }
-}
-
-module.exports = { Triangle, Square, Circle };
+  }
+  
+  // Export the Triangle, Square, and Circle classes as a module
+  module.exports = { Triangle, Square, Circle };
--- index.js
+++ index.js
@@ -1,27 +1,35 @@
+// Import necessary modules and classes
 const inquirer = require('inquirer');
 const fs = require('fs');
 const { Triangle, Square, Circle } = require("./Lib/shapes");
 
+// Define the Svg class
 class Svg {
   constructor() {
     this.textElement = '';
     this.shapeElement = '';
   }
+
+  // Return the SVG string
   render() {
     return `<svg version="1.1" width="300" height="200" viewBox="0 0 300 200" xmlns="http://www.w3.org/2000/svg">
       ${this.shapeElement}
       ${this.textElement}
     </svg>`;
   }
+
+  // Set the text element of the SVG
   setTextElement(text, color) {
     this.textElement = `<text x="150" y="160" font-size="30" text-anchor="middle" fill="${color}">${text}</text>`;
   }
+
+  // Set the shape element of the SVG
   setShapeElement(shape) {
     this.shapeElement = shape.render();
   }
 }
 
-//Array of questions for the user's input
+// Define an array of questions for the user's input
 const questions = [
   {
     type: "input",
@@ -54,18 +62,26 @@ const questions = [
 
 // Prompt the user with the questions and process their answers
 inquirer.prompt(questions).then((answers) => {
+  // Create a new instance of the Svg class
   const svg = new Svg();
+
+  // Set the text element of the SVG using the user's input
   svg.setTextElement(answers.text, answers["text-color"]);
+
+  // Set the shape element of the SVG based on the user's choice
   switch (answers["pixel-image"]) {
     case "Triangle":
-      svg.setShapeElement(new Triangle(0, 0, 0,answers["shape-color"]));
+      svg.setShapeElement(new Triangle(0, 0, 0, answers["shape-color"]));
       break;
     case "Square":
       svg.setShapeElement(new Square(0, 0, 0, answers["shape-color"]));
       break;
     case "Circle":
-      svg.setShapeElement(new Circle(100,100,100, answers["shape-color"]));      break;
+      svg.setShapeElement(new Circle(100,100,100, answers["shape-color"]));
+      break;
   }
+
+  // Write the SVG string to a file and log a success message
   fs.writeFile("logo.svg", svg.render(), (err) => {
     if (err) throw err;
     console.log("SVG file created successfully!");
