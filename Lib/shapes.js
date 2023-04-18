183ce9405e6f1c5b5b927ea362ddc141a78049b2
Asharamart2075@gmail.com <Asharamart2075@gmail.com>
2023-04-17 21:44:03 -0500

    FIXED DONT MESS WITH

--- Lib/shapes.js
+++ Lib/shapes.js
@@ -9,7 +9,7 @@ class Triangle {
   
   // Define the render method for the Triangle class, which returns a SVG polygon with the given color
   render() {
-  return <polygon points="150, 18 244, 182 56, 182" fill="${this.color}" />;
+  return `<polygon points="150, 18 244, 182 56, 182" fill="${this.color}"/>`;
   }
   }
   
@@ -24,7 +24,7 @@ class Triangle {
   
   // Define the render method for the Square class, which returns a SVG rectangle with the given color
   render() {
-  return <rect x="50" y="50" width="200" height="200" fill="${this.color}" />;
+  return `<rect x="50" y="50" width="200" height="200" fill="${this.color}"/>`;
   }
   }
   
@@ -39,7 +39,7 @@ class Triangle {
   
   // Define the render method for the Circle class, which returns a SVG circle with the given color and radius
   render() {
-  return <circle cx="${this.x + (this.size / 2)}" cy="${this.y + (this.size / 2)}" r="${this.size / 2}" fill="${this.color}" />;
+  return `<circle cx="${this.x + (this.size / 2)}" cy="${this.y + (this.size / 2)}" r="${this.size / 2}" fill="${this.color}"/>`;
   }
   }
   
